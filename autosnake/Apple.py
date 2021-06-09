250edde1369e601993af7a85e9f95e2e3927dc55
azizmousa <azizmousa1010@gmail.com>
2021-06-10 00:03:33 +0200

    create apple class

---
+++ autosnake/Apple.py
@@ -0,0 +1,17 @@
+import pygame
+
+
+class Apple:
+    def __init__(self, window, x=0, y=0, width=10, height=10):
+        self.__x = x
+        self.__y = y
+        self.__WIDTH = width
+        self.__HEIGHT = height
+        self.__window = window
+
+    def draw_apple(self):
+        apple = pygame.Rect((self.__x, self.__y), (self.__WIDTH, self.__HEIGHT))
+        pygame.draw.rect(self.__window, (255, 0, 0), apple)
+        pygame.display.update()
+
+
