b2a6c84399c12d4df40e0984fd9bb3d679a47254
azizmousa <azizmousa1010@gmail.com>
2021-06-10 00:04:08 +0200

    create game class

---
+++ autosnake/Game.py
@@ -0,0 +1,36 @@
+import pygame
+from pygame.locals import *
+from Snake import Snake
+from Apple import Apple
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.__main_window = pygame.display.set_mode((1000, 500))
+        self.__snake = Snake(self.__main_window)
+        self.__apple = Apple(self.__main_window)
+
+    def start(self):
+        running = True
+        while running:
+
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+                        continue
+                    elif event.key == K_LEFT:
+                        self.__snake.move_left()
+                    elif event.key == K_RIGHT:
+                        self.__snake.move_right()
+                    elif event.key == K_DOWN:
+                        self.__snake.move_down()
+                    elif event.key == K_UP:
+                        self.__snake.move_up()
+                elif event.type == QUIT:
+                    running = False
+                    continue
+
+            self.__snake.draw_snake()
+            self.__apple.draw_apple()
