d6cd10abbed4178c82d622c1df356f0d3e0b33ca
azizmousa <azizmousa1010@gmail.com>
2021-06-17 19:42:26 +0200

    create node class

---
+++ autosnake/Node.py
@@ -0,0 +1,16 @@
+class Node:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def get_x(self):
+        return self.__x
+
+    def get_y(self):
+        return self.__y
+
+    def get_coordinates(self):
+        return self.__x, self.__y
+
+    def __eq__(self, other):
+        return self.__x == other.__x and self.__y == other.__y
