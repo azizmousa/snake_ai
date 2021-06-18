0d2fc052a90aac1dfca026a62005c81f31b6a714
azizmousa <azizmousa1010@gmail.com>
2021-06-18 02:43:40 +0200

    override comparing, hashing method

--- autosnake/Node.py
+++ autosnake/Node.py
@@ -14,3 +14,9 @@ class Node:
 
     def __eq__(self, other):
         return self.__x == other.__x and self.__y == other.__y
+
+    def __str__(self):
+        return f"({self.__x}, {self.__y})"
+
+    def __hash__(self):
+        return hash((self.__x, self.__y))
