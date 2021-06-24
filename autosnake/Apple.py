769abef1eaa25bfc6440f746be382b24e3cc94aa
azizmousa <azizmousa1010@gmail.com>
2021-06-24 18:14:24 +0200

    scale apple coordinates to cover the graph nodes

--- autosnake/Apple.py
+++ autosnake/Apple.py
@@ -2,18 +2,19 @@ import pygame
 
 
 class Apple:
-    def __init__(self, window, x=0, y=0, width=10, height=10):
+    def __init__(self, window, x=0, y=0, width=10, height=10, scale=1):
         self.__x = x
         self.__y = y
         self.__WIDTH = width
         self.__HEIGHT = height
         self.__window = window
+        self.__scale = scale
 
     def get_coordinates(self):
         return self.__x, self.__y
 
     def draw(self):
-        apple = pygame.Rect((self.__x, self.__y), (self.__WIDTH, self.__HEIGHT))
+        apple = pygame.Rect((self.__x*self.__scale, self.__y*self.__scale), (self.__WIDTH, self.__HEIGHT))
         pygame.draw.rect(self.__window, (255, 0, 0), apple)
         # pygame.display.update()
 
