eaf7d1ea308f2557c18057eba67ac5969c8052c7
azizmousa <azizmousa1010@gmail.com>
2021-06-24 21:38:41 +0200

    check if the graph updated

--- autosnake/Walker.py
+++ autosnake/Walker.py
@@ -3,9 +3,18 @@ class Walker:
         self._graph = graph
         self._start = start
         self._target = target
+        self._graph_updated = True
 
     def get_next_direction(self):
         pass
 
-    def update_graph(self, graph):
+    def update_graph(self, graph, start, target):
         self._graph = graph
+        self._start = start
+        self._target = target
+        self._graph_updated = True
+
+    def is_graph_updated(self):
+        ret = self._graph_updated
+        self._graph_updated = False
+        return ret
