b17daa6fa2c0485defe7f9cbac738d538c5b6d11
azizmousa <azizmousa1010@gmail.com>
2021-06-27 18:08:27 +0200

    initialize the three algorithmes and assign bfs to the game

--- autosnake/main.py
+++ autosnake/main.py
@@ -1,5 +1,10 @@
 from Game import Game
-
+from BFS import Bfs
+from Dijikstra import Dijikstra
+from Astar import AStar
 if __name__ == '__main__':
-    game = Game(1000, 500)
+    bfs = Bfs(None, None, None)
+    a_star = AStar(None, None, None)
+    dij = Dijikstra(None, None, None)
+    game = Game(bfs, 1000, 500)
     game.start()
